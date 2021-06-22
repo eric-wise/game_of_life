9a631f6e632d8f1ec142c741ce3dccd9a935ff72
Eric Wise <ericwise@gmail.com>
2021-06-21 19:51:44 -0500

    Grid toggable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
+import produce from 'immer'
 
 const numRows = 50
 const numCols = 50
 
 function App() {
   const [grid, setGrid] = useState(() => {
-    
+
     const rows = [];
     for (let i = 0; i < numRows; i++ ) {
       rows.push(Array.from(Array(numCols), () => 0))
@@ -23,6 +24,13 @@ function App() {
         rows.map((col, k) => (
       <div 
       key={`${i}-${k}`}
+      onClick={() => {
+        const newGrid = produce(grid, gridCopy => {
+          gridCopy[i][k] = grid[i][k] ? 0 : 1;
+        })
+        setGrid(newGrid)
+        
+      }}
       style={{width: 20,
          height: 20, 
          backgroundColor: grid[i][k] ? "red" : undefined, 
